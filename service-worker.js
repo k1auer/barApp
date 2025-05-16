f5733a116ee78e799ffd838edba76910337e82ef
alexander.diehm <alexander.diehm@swarco.de>
2025-05-16 12:34:04 +0200

    auto update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'getraenkerechner-cache-v1';
+const CACHE_NAME = 'getraenkerechner-cache-v2';
 const urlsToCache = [
     '/',
     '/index.html',
@@ -14,5 +14,13 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+        )
     );
 });
